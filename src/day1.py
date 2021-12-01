67714ccc49b6de8182b9c2a7746ccd62ad4282bd
Yifan Xia <xiayifanisme@gmail.com>
2021-12-01 10:25:09 +0100

    finished day 1

---
+++ src/day1.py
@@ -0,0 +1,25 @@
+"""Advent of Code 2021: Day 1."""
+
+from typing import List
+
+def get_increases(measurements: List[int]) -> int:
+    increases = [
+        a < b for a, b in zip(measurements[:-1], measurements[1:])
+    ]
+    return sum(increases)
+
+def get_sum_of_three(measurements: List[int]) -> List[int]:
+    sums = [
+        a + b + c 
+        for a, b, c in zip(measurements[:-2], measurements[1:-1], measurements[2:])
+    ]
+    return sums
+
+if __name__ == "__main__":
+
+    with open("/home/yifan_xia/advent_of_code/2021/data/day1", "r") as input:
+        measurements = list(map(int, input.readlines()))
+    print(get_increases(measurements))
+
+    sums = get_sum_of_three(measurements)
+    print(get_increases(sums))
