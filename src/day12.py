35e1be5fe9dff7c502103b2a38b398e7fa210885
Yifan Xia <xiayifanisme@gmail.com>
2022-01-07 22:03:19 +0100

    finished day 12

---
+++ src/day12.py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+from typing import DefaultDict, List, Set
+
+def find_path(start: str, visited: Set, adjacency_list: DefaultDict[str, Set], allow_twice: bool = False) -> List[List[str]]:
+    if start == "end":
+        return [[start]]
+    res = []
+    neighbours = adjacency_list[start]
+    for node in neighbours:
+        if node.islower() and node in visited:
+            if not allow_twice or node == "start":
+                continue
+            allow_twice_next = False
+        else:
+            allow_twice_next = allow_twice
+        paths = [
+            [start] + path
+            for path in find_path(node, visited.union({start}), adjacency_list, allow_twice_next)
+        ]
+        res = res + paths
+    return res
+        
+
+
+
+def main():
+
+    adjacency_list = defaultdict(set)
+
+    with open("../data/day12", "r") as file:    
+        line = file.readline()
+        while line:
+            a, b = line.strip().split("-")
+            adjacency_list[a].add(b)
+            adjacency_list[b].add(a)
+            line = file.readline()
+    
+    import pprint
+    res_1 = find_path("start", set(), adjacency_list)
+    print(len(res_1))
+    res_2 = find_path("start", set(), adjacency_list, True)
+    print(len(res_2))
+
+if __name__ == "__main__":
+    import os
+    try:
+        os.chdir(os.path.dirname(__file__))
+        main()
+    finally:
+        os.chdir("../")
