3155f22596a67004aeda3cddfffd6523e076c152
Yifan Xia <xiayifanisme@gmail.com>
2022-01-12 11:27:27 +0100

    day 14

--- src/day14.py
+++ src/day14.py
@@ -1,5 +1,5 @@
 from typing import Dict, List
-from collections import Counter
+from collections import Counter, defaultdict
 
 def unit_polymerize(left: str, rules: Dict[str, str]) -> List[str]:
     right = rules.get(left)
@@ -37,10 +37,25 @@ def main():
     polymer_counts = Counter(list(new_template)).most_common()
     print(polymer_counts[0][1] - polymer_counts[-1][1])
 
-    new_template = polymerize(template, reactions, 40)
-    polymer_counts = Counter(list(new_template)).most_common()
-    print(polymer_counts[0][1] - polymer_counts[-1][1])
-
+    # Part 2
+    # Solution by dynamical programming
+    # Count pairs of each step to avoid producing lengthy strings at large steps
+    pairs = [template[i-1:i+1] for i in range(1, len(template))]
+    pair_counts = Counter(pairs)
+    print(pair_counts)
+    first_letter = template[0]
+    for _ in range(40):
+        new_pair_counts = defaultdict(int)
+        for p in pair_counts:
+            if r := reactions.get(p):
+                new_pair_counts["".join([p[0], r])] += pair_counts[p]
+                new_pair_counts["".join([r, p[1]])] += pair_counts[p]
+        pair_counts = new_pair_counts
+    letter_counts = defaultdict(int, {first_letter: 1})
+    for pair in pair_counts:
+        letter_counts[pair[1]] += pair_counts[pair]
+    letter_counts = letter_counts.values()
+    print(max(letter_counts) - min(letter_counts))
 
 
         
